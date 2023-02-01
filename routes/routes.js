c38fb4cfcfe7d7eda9471980e6c2b04a00984881
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-02-01 09:56:08 +0100

    changed api url

--- routes/routes.js
+++ routes/routes.js
@@ -3,19 +3,19 @@ const db = require('../data/database');
 const router = app => {
   app.get('/', (request, response) => {
       response.send({
-          message: 'Node.js and Express REST API'
+          message: 'REST API for RS-Clone'
       });
   });
 
-  app.get('/brands', (request, response) => {
+  app.get('/api/brands', (request, response) => {
 		response.send(db.brands);
 	});
 
-  app.get('/flavors', (request, response) => {
+  app.get('/api/flavors', (request, response) => {
 		response.send(db.flavors);
 	});
 
-  app.get('/mixes', (request, response) => {
+  app.get('/api/mixes', (request, response) => {
 		response.send(db.mixes);
 	});
 }
