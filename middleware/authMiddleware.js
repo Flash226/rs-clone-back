5eca42b08a5eeec96fa238bba8ccefe87d8d5e90
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-02-03 20:57:50 +0100

    fixed errors

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const {secret} = require('../configJwt');
+
+module.exports = function (req, res, next) {
+    if (req.method === "OPTIONS") {
+        next();
+    }
+    try {
+        if (!req.headers.authorization) {
+            return res.status(403).json({message: "User not authorized"});
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(403).json({message: "User not authorized"});
+        }
+        const decodedData = jwt.verify(token, secret);
+        req.user = decodedData;
+        next();
+    } catch (e) {
+        console.log(e);
+        return res.status(403).json({message: "User not authorized"});
+    }
+};
--- routes/routesAuth.js
+++ routes/routesAuth.js
@@ -2,12 +2,13 @@ const Router = require('express');
 const router = new Router();
 const controller = require('../components/authController');
 const { check } = require('express-validator');
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', [
   check('username', 'Username cannot be empty').notEmpty(),
   check('password', 'Password must be more than 5 and less than 12 characters').isLength({ min: 5 })
 ], controller.registration);
 router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+router.get('/users', authMiddleware, controller.getUsers);
 
 module.exports = router;
